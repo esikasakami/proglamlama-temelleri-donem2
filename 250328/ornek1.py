4203ea9cab5fb1e2db306c9546f380c031195712
esikasakami <esilasorkuncuk@icloud.com>
2025-03-28 11:43:19 +0300

    Add files via upload

---
+++ 250328/ornek1.py
@@ -0,0 +1,7 @@
+islemci=int(input("islemciyi giriniz:"))
+ram=int(input("ram belleği giriniz:"))
+if islemci>=7 and ram>=8:
+    print("kurulum icin uygundur")    
+else:
+    print("uygun degildir")
+        
---
+++ 250328/ornek2.py
@@ -0,0 +1,5 @@
+hava_sicakligi=int(input("hava sıcaklıgını giriniz"))
+if hava_sicakligi <4:
+    print("dikkat buzlanma seviyesi")
+else:
+    print("iyi yolculuklar")
